4318861eded77a6bc2f421859b52c82127cd9e52
Bridger Middleton <bmiddy3@gmail.com>
2026-01-25 16:20:34 -0500

    added some comments

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -3,13 +3,20 @@ import { Box, Button, Container, TextField, Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 export const Signup = () => {
+
+  // Form data state
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     password: ''
   });
+
+  // need this to navigate to other routes
   const navigate = useNavigate();
 
+
+  // this tracks changes in the input fields.  essentially we get the name and value from event.targe.  when we set the form data
+  // we use the spread operator to keep the state that didn't change and set the name that changed to the new value
   const handleFormChange = (event) => {
     const {name, value} = event.target;
 
@@ -19,17 +26,32 @@ export const Signup = () => {
     }));
   };
 
+
+  // handle form submission
+
   const handleSubmit = async (event) => {
+
+    // this prevents the default behavior from happening.  when you submit a form in the browser, it typically wants to send an HTTP request and
+    // reload the page.  this stops that so that we can write our own code to handle the submission
     event.preventDefault();
     try {
+
+      // hit the server at the /api/register route
       const response = await fetch("/api/register", {
+
+        // this is a POST request
         method: "POST",
+
+        // specifiying that this will be json
         headers: {
           'Content-Type': 'application/json',
         },
+
+        // jsonifying the form data to be sent to the server
         body: JSON.stringify(formData),
       });
 
+      // if our post request didn't throw an error, then we check the response message and if it is success we navigate to the home page
       if (response.ok)
       {
         const result = await response.json();
@@ -39,6 +61,8 @@ export const Signup = () => {
           navigate('/home')
         }
       }
+
+      // otherwise we return some error logging
       else{
         console.error('Submission failed', response.statusText);
       }
--- server/app.py
+++ server/app.py
@@ -5,20 +5,28 @@ import bcrypt
 import sqlite3
 from helpers import getDB, createCursor
 
-
+# instantiate the app
 app = Flask(__name__)
 
+# allows us to make requests to other urls (since our frontend and backend are hosted on different urls, we need to do this)
 CORS(app)
 
+# register
 @app.route('/api/register', methods=["POST"])
 def register():
 
     if request.method == "POST":
+
+        # we connect to our DB and our cursor.  we then checkout the json that we got sent by creating that data variable
         db = getDB()
         cursor = createCursor(db)
         data = request.get_json()
         print(data)
+
+        # if data isn't empty...
         if data:
+
+            # basically get all of our form elements, hash a password, and then...
             name = data.get("name")
             print("name",name)
             email = data.get("email")
@@ -29,20 +37,25 @@ def register():
             password_bytes = password_str.encode("utf-8")
             hashed_password = bcrypt.hashpw(password_bytes, salt)
 
+
+            # we try to execute the SQL command.  we then commit it to our db, create a response message, and then return that response message as json
             try:
                 cursor.execute("INSERT INTO users (name, email, hash) VALUES (?, ?, ?)", (name, email, hashed_password))
                 db.commit()
                 response = {"message": "success"}
                 return jsonify(response);
+
+                # if it didn't work, we throw a response with the error message
             except Exception as e:
                 response = {"message": e}
                 return jsonify(response)
+
+                # after all is said and done, we need to close our cursor and our DB so we don't run into the threading issue
             finally:
                 cursor.close()
                 db.close()
 
-        else:
-            print("fuck")
+        return
     
 
 
