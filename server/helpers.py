f9a2f05ae28ccd30861e02ac1e965baa6fa994b9
Bridger Middleton <bmiddy3@gmail.com>
2026-02-14 15:42:53 -0500

    fixed path issues for the DB

--- server/helpers.py
+++ server/helpers.py
@@ -1,7 +1,11 @@
 import sqlite3
+import os
 
+# get directory where helpers.py lives
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 def getDB():
-    connection = sqlite3.connect("simply-lifts.db")
+    db_path = os.path.join(BASE_DIR, "simply-lifts.db")
+    connection = sqlite3.connect(db_path)
     return connection
 
 def createCursor(db):
